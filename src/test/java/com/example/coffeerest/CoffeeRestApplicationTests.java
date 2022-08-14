e985577f52b2edfdf414708cc592704e26293098
zeyad taher <zeyadtaher16@gmail.com>
2022-08-14 17:21:09 +0200

    initial commit

---
+++ src/test/java/com/example/coffeerest/CoffeeRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffeerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
